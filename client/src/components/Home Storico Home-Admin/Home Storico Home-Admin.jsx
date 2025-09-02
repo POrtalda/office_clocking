1dab4c77551ac90a278861ff56503f95498ceb89
POrtalda <paoloortalda@gmail.com>
2025-09-02 22:02:41 +0200

    components

---
+++ client/src/components/Home Storico Home-Admin/Home Storico Home-Admin.jsx
@@ -0,0 +1,10 @@
+import './Home Storico Home-Admin.css';
+
+export default function Home Storico Home-Admin() {
+
+    return(
+        <>
+            <p>this is Home Storico Home-Admin</p>
+        </>
+    );
+}
